a521df20d36e90f3078c45effc953190a1f3e830
tonhodofus <marcospad92@gmail.com>
2019-10-26 19:28:06 -0300

    - StartMenu.unity

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayGame()
+	{
+		//this will call the next scene, you can set this on File>BuildSettings, 
+		//add this on position 0 and on position 1 the first scene
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void QuitGame ()
+ 	{
+ 		Debug.Log ("QUIT!");
+  		Application.Quit();
+ 	}
+  
+}
+	
+
